b360a456db9e106df85d949c5acb69ceabbdeae0
guanqian <992678108@qq.com>
2017-05-17 16:54:07 +0800

    directive

--- app/js/controllers/test1Ctrl.js
+++ app/js/controllers/test1Ctrl.js
@@ -4,5 +4,17 @@
  * Created by Administrator on 2017/5/10.
  */
 iasApp.controller('test1Ctrl', ['$scope', function ($scope) {
-  console.log('test1Ctrl');
-}]);
+    console.log('test1Ctrl');
+}]).directive('myDirective', function ($rootScope) {
+    return {
+        priority: 1000,
+        restrict: 'A',
+        replace: true,
+        scope: {
+            myDirective: '='
+        },
+        template: '<p>from myDirective:{{myDirective}}</p>'
+    };
+}).controller('myController', function ($scope) {
+    $scope.content = 'from controller';
+});
